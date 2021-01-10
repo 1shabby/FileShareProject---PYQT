1b5ba0daec8421487f49c33a2cea926f3f357e85
1shabby <1shabbymn@gmail.com>
2021-01-10 14:37:40 -0600

    Implemented back end modification of the config file. Still need to get the GUI to update properly.

--- MultiWindowGUI.py
+++ MultiWindowGUI.py
@@ -70,15 +70,17 @@ class Edit_Config_Page(QtWidgets.QMainWindow, EditConfig.Ui_EditConfigWindow):
     switch_wndow = QtCore.pyqtSignal()
 
     def __init__(self, parent=None):
+
+        ConfigFilePath = self.config_check()
+        Config_Parser = CONFIG_PARSER
+
         super(Edit_Config_Page, self).__init__(parent)
         self.setupUi(self)
-        ConfigFilePath = self.config_check()
-        Config_Parser = ConfigParser()
         Config_Parser.read(ConfigFilePath)
         connectionCount = Config_Parser.get("Connection Count", "count")
         autoLoad = Config_Parser.get("settings", "auto_connect")
 
-        if autoLoad == "true":
+        if autoLoad == "True":
             self.AutoLoadButton.setChecked(True)
 
         self.GetConnectionInfo(Config_Parser, connectionCount)
@@ -88,6 +90,8 @@ class Edit_Config_Page(QtWidgets.QMainWindow, EditConfig.Ui_EditConfigWindow):
         self.lineEdit_2.textChanged.connect(self.InputFieldsChanged)
         self.ServerIPLineEdit.textChanged.connect(self.InputFieldsChanged)
         self.SaveButton.clicked.connect(self.EditConnection)
+        self.AutoLoadButton.toggled.connect(self.SetAutoLoad)
+
         self.EditConfigController = Controller()
 
     def BackButtonPressed(self):
@@ -104,8 +108,8 @@ class Edit_Config_Page(QtWidgets.QMainWindow, EditConfig.Ui_EditConfigWindow):
             QMessageBox.setIcon(QMessageBox.Critical)
             QMessageBox.show()
             exit()
-        else:
-            return ConfigFilePath
+
+        return ConfigFilePath
 
     def GetConnectionInfo(self, Config_Parser, connectionCount):
         count = 1
@@ -129,21 +133,32 @@ class Edit_Config_Page(QtWidgets.QMainWindow, EditConfig.Ui_EditConfigWindow):
     def InputFieldsChanged(self):
         if self.ServerIPLineEdit.text() != "" and self.lineEdit_2.text() != "":
             self.AddNewConnectionButton.setEnabled(True)
+            if self.DisplayListWidget.currentRow() > -1:
+                self.SaveButton.setEnabled(True)
         else:
             self.SaveButton.setEnabled(False)
             self.AddNewConnectionButton.setEnabled(False)
 
     def SetAutoLoad(self):
-        AutoLoad = ""
-        if self.AutoLoadButton.isChecked == True:
-            AutoLoad = "True"
-        elif self.AutoLoadButton.isChecked == False:
-            AutoLoad = "False"
-        print(AutoLoad)
+        ConfigFilePath = self.config_check()
+        Config_Parser = CONFIG_PARSER
 
+        AutoLoad = ""
+        if self.AutoLoadButton.isChecked() == True:
+            AutoLoad = "True"
+
+        elif self.AutoLoadButton.isChecked() == False:
+            AutoLoad = "False"
+
+        Config_Parser.set('settings', 'auto_connect', AutoLoad)
+        with open(ConfigFilePath, 'w') as configfile:
+            Config_Parser.write(configfile)
+
+    # TODO: Implement the back end modification of the ini file and update the GUI.
     def RemoveConnection(self):
 
         return
+    # TODO: Implement the back end modification of the ini file and update the GUI.
 
     def AddConnection(self):
 
@@ -154,11 +169,21 @@ class Edit_Config_Page(QtWidgets.QMainWindow, EditConfig.Ui_EditConfigWindow):
         ServerPort = self.lineEdit_2.text()
         IPValid = self.ip_check(ServerIP)
         PortValid = self.port_check(ServerPort)
-        ConfigParser = self.Config_Parser
-        ConfigFilePath = self.ConfigFilePath
+        SelectedRow = self.DisplayListWidget.currentRow() + 1
+
+        Config_Parser = CONFIG_PARSER
+        ConfigFilePath = self.config_check()
+        ConnectionCount = Config_Parser.get("Connection Count", "count")
 
         if IPValid == True and PortValid:
-            print("Valid IP and Port!")
+            Config_Parser.set(
+                'Connection ' + str(SelectedRow), 'ip', str(ServerIP))
+            Config_Parser.set('Connection ' + str(SelectedRow),
+                              'port', str(ServerPort))
+            with open(ConfigFilePath, 'w') as configfile:
+                Config_Parser.write(configfile)
+            # self.DisplayListWidget.clear()
+            #self.GetConnectionInfo(ConfigParser, ConnectionCount)
 
     def ip_check(self, IP_Text):
         returnVal = False
